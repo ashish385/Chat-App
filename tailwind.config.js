324090c5e6f68582a8f2965ec93f93cf05dcadc7
Ashish Soni <4840asoni@gmail.com>
2023-08-17 13:04:55 +0530

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: 'jit',
+  purge: [
+     './public/**/*.html',
+     './src/**/*.{js,jsx,ts,tsx,vue}',
+   ],
+  content: ['./public/**/*.html',"./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
